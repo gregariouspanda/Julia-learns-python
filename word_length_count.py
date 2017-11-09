805e1b09f60c9f82036001b2987d3e2a69fbb91f
Julia D. Marden <julia@marden.org>
2017-11-09 00:09:30 -0500

    Update word_length_count.py

--- word_length_count.py
+++ word_length_count.py
@@ -12,16 +12,15 @@ for word in specific_word_count.keys():
     else:
         word_lengths[len(word)] += specific_word_count[word]
 
-objects = word_lengths.keys()
-y_pos = np.arange(len(objects))
-performance = word_lengths.values()
+sorted_keys = list(sorted(word_lengths.keys()))
+value = []
+for key in sorted_keys:
+	value.append(word_lengths[key])
 
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.xlabel('Number of words')
+y_pos = np.arange(len(value))
+
+plt.bar(y_pos, value, align='center', alpha=0.5)
+plt.xticks(y_pos, sorted_keys)plt.xlabel('Number of words')
 plt.ylabel('Length of word')
 plt.title('Number of words of a certain length')
-
-# if __name__ == '__main__':
-#     main()
 plt.show()
